fc950cdd9b6701b1640ce19dc4b7319c18241d7b
lance aziz mac <lance.aziz@cypodsolutions.com>
2025-07-21 17:10:44 +0300

    finals beta done

--- app/components/finalMatchCard/finalMatchCard.js
+++ app/components/finalMatchCard/finalMatchCard.js
@@ -1,39 +1,36 @@
 import React from 'react'
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faHourglassStart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from './finalMatchcardStyles.module.css'
 
 function FinalMatchCard({ match }) {
     function getAvatarText(name) {
-        if (!name) return "";
+        if (!name) return null;
         return name
             .split(" ")
             .map(word => word[0])
             .join("")
             .toUpperCase();
     }
+
     return (
         <div className='bg-white text-dark d-flex justify-content-center align-items-center gap-3 rounded-4 pt-3'>
-            <div className={`d-flex flex-column justify-content-between align-items-center ${match.winner == null || match.winner == match.team1.name ? "" : styles["dark-strike"]}`}>
-                <h2 className={`${match.winner == null ? 'bg-secondary' : match.winner !== match.team1.name ? 'bg-dark' : 'bg-success'} text-white p-4 rounded-4`}>
-                    {getAvatarText(match.team1.name)}
-                </h2>
-                <p>{match.team1.name}</p>
-                <p>
-                    {match.team1.score}
-                    {match.team1.penalties_score != null && ` (${match.team1.penalties_score})`}
-                </p>
-            </div>
-            <div className={`d-flex flex-column justify-content-between align-items-center ${match.winner == null || match.winner == match.team2.name ? "" : styles["dark-strike"]}`}>
-                <h2 className={`${match.winner == null ? 'bg-secondary' : match.winner !== match.team2.name ? 'bg-dark' : 'bg-success'} text-white p-4 rounded-4`}>
-                    {getAvatarText(match.team2.name)}
-                </h2>
-                <p>{match.team2.name}</p>
-                <p>
-                    {match.team2.score}
-                    {match.team2.penalties_score != null && ` (${match.team2.penalties_score})`}
-                </p>
-            </div>
+            {match.teams.map((team, idx) => (
+                <div
+                    key={team.key || idx}
+                    className={`d-flex flex-column justify-content-between align-items-center ${match.winner == null || match.winner === team.name ? "" : styles["dark-strike"]}`}>
+                    <h2 className={`${match.winner == null ? 'bg-secondary' : match.winner !== team.name ? 'bg-dark' : 'bg-success'} text-white p-4 rounded-4`}>
+                        {getAvatarText(team.name) == null
+                            ? <FontAwesomeIcon icon={faHourglassStart} className='text-white' />
+                            : getAvatarText(team.name)}
+                    </h2>
+                    <p>{team.name}</p>
+                    <p>
+                        {team.score}
+                        {team.penalties_score != null && ` (${team.penalties_score})`}
+                    </p>
+                </div>
+            ))}
         </div>
     )
 }
--- app/tempdata.js
+++ app/tempdata.js
@@ -1,29 +1,37 @@
 export const finalMatches = {
-    
+
     // Quarter Finals (4 matches)
     Quarter_finals: [
         {
             id: 1,
-            team1: { name: "Team A", score: 2, penalties_score: null },
-            team2: { name: "Team B", score: 1, penalties_score: null },
+            teams: [
+                { name: "Team A", score: 2, penalties_score: null },
+                { name: "Team B", score: 1, penalties_score: null },
+            ],
             winner: "Team A",
         },
         {
             id: 2,
-            team1: { name: "Team C", score: 0, penalties_score: 4 },
-            team2: { name: "Team D", score: 0, penalties_score: 5 },
+            teams: [
+                { name: "Team C", score: 0, penalties_score: 4 },
+                { name: "Team D", score: 0, penalties_score: 5 },
+            ],
             winner: "Team D",
         },
         {
             id: 3,
-            team1: { name: "Team E", score: 3, penalties_score: null },
-            team2: { name: "Team F", score: 2, penalties_score: null },
+            teams: [
+                { name: "Team E", score: 3, penalties_score: null },
+                { name: "Team F", score: 2, penalties_score: null },
+            ],
             winner: "Team E",
         },
         {
             id: 4,
-            team1: { name: "Team G", score: 1, penalties_score: null },
-            team2: { name: "Team H", score: 2, penalties_score: null },
+            teams: [
+                { name: "Team G", score: 1, penalties_score: null },
+                { name: "Team H", score: 2, penalties_score: null },
+            ],
             winner: "Team H",
         },
     ],
@@ -32,30 +40,38 @@ export const finalMatches = {
     semi_finals: [
         {
             id: 5,
-            team1: { name: "", score: 0, penalties_score: null },
-            team2: { name: "", score: 0, penalties_score: null },
+            teams: [
+                { name: "", score: 0, penalties_score: null },
+                { name: "", score: 0, penalties_score: null },
+            ],
             winner: null,
         },
         {
             id: 6,
-            team1: { name: "", score: 0, penalties_score: null },
-            team2: { name: "", score: 0, penalties_score: null },
+            teams: [
+                { name: "", score: 0, penalties_score: null },
+                { name: "", score: 0, penalties_score: null },
+            ],
             winner: null
         },
     ],
 
-    // Final (2 matchs)
+    // Final (2 matches)
     finals: [
         {
             id: 7,
-            team1: { name: "", score: 0, penalties_score: null },
-            team2: { name: "", score: 0, penalties_score: null },
+            teams: [
+                { name: "", score: 0, penalties_score: null },
+                { name: "", score: 0, penalties_score: null },
+            ],
             winner: null,
         },
         {
             id: 8,
-            team1: { name: "", score:0, penalties_score: null },
-            team2: { name: "", score: 0, penalties_score: null },
+            teams: [
+                { name: "", score: 0, penalties_score: null },
+                { name: "", score: 0, penalties_score: null },
+            ],
             winner: null,
         },
     ]
